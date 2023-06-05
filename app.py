3098dab8fbc878fb048c08c4068e02a47699e183
unknown <novipurmita@gmail.com>
2023-06-05 07:58:35 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,35 @@
+import os
+from os.path import join, dirname
+from doteny import load_dotenv
+from flask import Flask, render_template, request, jsonify
+from pymongo import Mongoclient
+dotenv_path = join(dirname(_file_), "env")
+load_dotenv(dotenv_path)
+MONGODB_URI = os.environ.get("MONGODB_URI")
+DB_NAME = os.environ.get("DB_NAME")
+client = MongoClient(MONGODB_URI) 
+db = client [DB_NAME]
+app Flask(_name_)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route("/homework", methods=["POST"])
+def homework_post():
+    name_receive = request.form['name_give']
+    comment_receive = request.form['comment_give']
+    doc = {
+        'name': name_receive,
+        'comment': comment_receive
+    }
+    db.fanmessages.insert_one(doc)
+    return jsonify({'msg':'POST request!'})
+
+@app.route("/homework", methods=["GET"])
+def homework_get():
+    message_list = list(db.fanmessages.find({}, {'_id': False}))
+    return jsonify({'messages': message_list})
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
